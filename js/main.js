f51e003a1c55192ecb68d67362289693e990a18a
Carlo Gonzales <gon1387@gmail.com>
2013-02-25 09:50:15 +0800

    Adds the main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+requirejs.config({
+    paths: {
+        jquery: 'libs/jquery',
+        underscore: 'libs/underscore',
+        backbone: 'libs/backbone'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+        'jquery': {
+            exports: '$'
+        }
+    }
+});
 
+require(['app'] , function(App){
+    App.initialize();
+});
