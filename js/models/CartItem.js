5820f81523eb3a2d8cc0e43e0367e00362392ed0
Carlo Gonzales <gon1387@gmail.com>
2013-02-27 09:53:35 +0800

    Adds Validation on the Cart Item Model

--- js/models/CartItem.js
+++ js/models/CartItem.js
@@ -1,9 +1,22 @@
-define(['jquery','underscore','backbone'], function($, _, Backbone){
+define(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
     var CartItem = Backbone.Model.extend({
         defaults: {
-                name: 'none',
+                name: 'n/a',
                 quantity: 0,
                 price: 0
+        },
+        validate: function(attr){
+            if (!_.isString(attr.name)) {
+                return 'CartItem.name should be a string';
+            }
+            
+            if (!_.isNumber(attr.quantity)) {
+                return 'CartItem.name should be a number'
+            }
+            
+            if (!_.isNumber(attr.price)) {
+                return 'CartItem.price should be a number'
+            }
         }
     });
     
