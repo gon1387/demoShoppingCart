abae4aebcc802f3ef3fa7ecc946756053baaaea1
Carlo Gonzales <gon1387@gmail.com>
2013-02-27 09:53:01 +0800

    Adds Validation on the Shop Item Model

--- js/models/ShopItem.js
+++ js/models/ShopItem.js
@@ -3,9 +3,29 @@ define(['jquery','underscore','backbone'], function($, _, Backbone){
         defaults: {
             name: 'n/a',
             description: 'n/a',
-            price: '0.00',
+            price: 0,
             category: '',
+            //TODO: Look for a placeholder photo and change the default value
+            //  as it's path
             photo: ''
+        },
+        validate: function(attr){
+            if (!_.isString(attr.name)) {
+                return 'CartItem.name should be a string';
+            }
+            
+            if (!_.isString(attr.description)) {
+                return 'CartItem.name should be a string'
+            }
+            
+            if (!_.isNumber(attr.price)) {
+                return 'CartItem.price should be a number'
+            }
+            
+            if (!_.isString(attr.category)) {
+                return 'CartItem.price should be a string'
+            }
+            //TODO: Create a validation for photo url
         }
     });
     return ShopItem;
