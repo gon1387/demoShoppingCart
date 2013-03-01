a4753b6a84c3a06f3aae54511671db05a66d8b22
Carlo Gonzales <gon1387@gmail.com>
2013-03-01 23:33:33 +0800

    Adds a Register Method in Globals Module

--- js/utils/Globals.js
+++ js/utils/Globals.js
@@ -9,8 +9,33 @@ define(function(require, exports, module) {
     ShoppingCart = exports.ShoppingCart = global.ShoppingCart || (global.ShoppingCart = {
         Models: {},
         Collections: {},
-        Views: {}
+        Views: {
+            Pages: {},
+            Widgets: {},
+            Items: {}
+        }
     }); 
     
+    /**
+     * Registers a namespace and object in the global context
+     * 
+     * @param namespace string
+     * @param obj object
+     */
+    function register(namespace, obj){
+        var loc=ShoppingCart,
+            nmspc = namespace.split('/'),
+            nmspcLen = nmspc.length;
+            
+        _.each(nmspc, function(pckg,idx){
+            if(loc[pckg] && idx < 3){
+                loc = loc[pckg];
+            } else if(nmspcLen>1){
+                loc[pckg] = obj;
+            }
+        });
+    };
+    
+    exports.register = register;
     exports.global = global;
 });
