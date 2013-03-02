6ed3a4ea5a2107af149f31846e739b385a3a7602
Carlo Gonzales <gon1387@gmail.com>
2013-03-03 06:44:30 +0800

    Fixes Events initialization.

--- js/utils/Events.js
+++ js/utils/Events.js
@@ -5,7 +5,7 @@ define(function(require, exports, module) {
         Backbone = require('backbone'),
         ShoppingCart = require('utils/Globals').ShoppingCart;
         
-    ShoppingCart.EVT = ShoppingCart.EVT || _.extend(EVT, Backbone.Events);
+    ShoppingCart.EVT = ShoppingCart.EVT || _.extend({}, Backbone.Events);
     
     return ShoppingCart.EVT
 });
